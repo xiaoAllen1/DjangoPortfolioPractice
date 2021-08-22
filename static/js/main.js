696d88c59a592cc0465a9acc766c12e13d04c5ca
tech-jamara <jamarajames899@gmail.com>
2021-08-23 02:39:34 +0300

    portfolioghkk

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,73 @@
 //         });
 //     }
 // });
+
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'icofont-moon'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'brightness-low' : 'bx-moon'
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'brightness-low' ? 'add' : 'remove'](iconTheme)
+
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
+
+
+
+var scrolltoOffset = $('#header').outerHeight() - 16;
+if (window.matchMedia("(max-width: 991px)").matches) {
+    scrolltoOffset += 16;
+}
+$(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        e.preventDefault();
+        var target = $(this.hash);
+        if (target.length) {
+
+            var scrollto = target.offset().top - scrolltoOffset;
+
+            if ($(this).attr("href") == '#header') {
+                scrollto = 0;
+            }
+
+            $('html, body').animate({
+                scrollTop: scrollto
+            }, 200, 'easeInOutExpo');
+
+            if ($(this).parents('.nav-menu, .mobile-nav').length) {
+                $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+                $(this).closest('li').addClass('active');
+            }
+
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+                $('.mobile-nav-overly').fadeOut();
+            }
+            return false;
+        }
+    }
+});
+
 $(document).ready(function() {
 
 
