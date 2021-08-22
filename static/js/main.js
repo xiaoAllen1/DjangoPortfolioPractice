1d848d412df0cdd1a734dd89e56e70c5b462fd8e
tech-jamara <jamarajames899@gmail.com>
2021-08-22 23:52:54 +0300

    portfolio2

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,36 @@
     });
 
 
-    // Smooth scroll for the navigation menu and links with .scrollto classes
+    const themeButton = document.getElementById('theme-button')
+    const darkTheme = 'dark-theme'
+    const iconTheme = 'icofont-moon'
+
+
+    const selectedTheme = localStorage.getItem('selected-theme')
+    const selectedIcon = localStorage.getItem('selected-icon')
+
+
+    const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
+    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'brightness-low' : 'bx-moon'
+
+
+    if (selectedTheme) {
+        document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](darkTheme)
+        themeButton.classList[selectedIcon === 'brightness-low' ? 'add' : 'remove'](iconTheme)
+
+    }
+
+    themeButton.addEventListener('click', () => {
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+
+        localStorage.setItem('selected-theme', getCurrentTheme())
+        localStorage.setItem('selected-icon', getCurrentIcon())
+
+    })
+
+
+
     var scrolltoOffset = $('#header').outerHeight() - 16;
     if (window.matchMedia("(max-width: 991px)").matches) {
         scrolltoOffset += 16;
