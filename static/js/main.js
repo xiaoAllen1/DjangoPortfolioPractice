90e0b9b6a071262f94b730902462429b26ff835c
tech-jamara <jamarajames899@gmail.com>
2021-08-23 03:03:01 +0300

    portfolioghkmkm

--- static/js/main.js
+++ static/js/main.js
@@ -1,111 +1,17 @@
-// Preloader
-// $(window).on('load', function() {
-//     if ($('#preloader').length) {
-//         $('#preloader').delay(100).fadeOut('slow', function() {
-//             $(this).remove();
-//         });
-//     }
-// });
+!(function($) {
+    "use strict";
 
-
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'icofont-moon'
-
-
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'brightness-low' : 'bx-moon'
-
-
-if (selectedTheme) {
-    document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'brightness-low' ? 'add' : 'remove'](iconTheme)
-
-}
-
-themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-
-})
-
-
-
-var scrolltoOffset = $('#header').outerHeight() - 16;
-if (window.matchMedia("(max-width: 991px)").matches) {
-    scrolltoOffset += 16;
-}
-$(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        e.preventDefault();
-        var target = $(this.hash);
-        if (target.length) {
-
-            var scrollto = target.offset().top - scrolltoOffset;
-
-            if ($(this).attr("href") == '#header') {
-                scrollto = 0;
-            }
-
-            $('html, body').animate({
-                scrollTop: scrollto
-            }, 200, 'easeInOutExpo');
-
-            if ($(this).parents('.nav-menu, .mobile-nav').length) {
-                $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-                $(this).closest('li').addClass('active');
-            }
-
-            if ($('body').hasClass('mobile-nav-active')) {
-                $('body').removeClass('mobile-nav-active');
-                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-                $('.mobile-nav-overly').fadeOut();
-            }
-            return false;
+    // Preloader
+    $(window).on('load', function() {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
         }
-    }
-});
-
-$(document).ready(function() {
+    });
 
 
-    const themeButton = document.getElementById('theme-button')
-    const darkTheme = 'dark-theme'
-    const iconTheme = 'icofont-moon'
-
-
-    const selectedTheme = localStorage.getItem('selected-theme')
-    const selectedIcon = localStorage.getItem('selected-icon')
-
-
-    const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
-    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'brightness-low' : 'bx-moon'
-
-
-    if (selectedTheme) {
-        document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](darkTheme)
-        themeButton.classList[selectedIcon === 'brightness-low' ? 'add' : 'remove'](iconTheme)
-
-    }
-
-    themeButton.addEventListener('click', () => {
-        document.body.classList.toggle(darkTheme)
-        themeButton.classList.toggle(iconTheme)
-
-        localStorage.setItem('selected-theme', getCurrentTheme())
-        localStorage.setItem('selected-icon', getCurrentIcon())
-
-    })
-
-
-
+    // Smooth scroll for the navigation menu and links with .scrollto classes
     var scrolltoOffset = $('#header').outerHeight() - 16;
     if (window.matchMedia("(max-width: 991px)").matches) {
         scrolltoOffset += 16;
@@ -188,10 +94,10 @@ $(document).ready(function() {
 
 
 
-    // AOS.init({
-    //     duration: 1500,
-    //     once: true
-    // });
+    AOS.init({
+        duration: 1500,
+        once: true
+    });
 
     $(function() {
         $(document).scroll(function() {
@@ -200,11 +106,11 @@ $(document).ready(function() {
         });
     });
 
-})
+})(jQuery);
 
-// $(function() {
-//     $(document).scroll(function() {
-//         var $nav = $(".navbur");
-//         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-//     });
-// });
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbur");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
--- static/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.button-left').click(function(){
-        $('.sidebar').toggleClass('fliph');
-    });
-     
-   
- });
-
