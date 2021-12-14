1e26e1129628bf221e6c4a531ba559672e7d6149
tech-jamara <jamarajames899.com>
2021-12-14 17:36:22 +0300

    5th commit2

--- feeds/models.py
+++ feeds/models.py
@@ -6,7 +6,7 @@ from ckeditor.fields import RichTextField
 
 class PersonalInformation(models.Model):
     name_complete = models.CharField(max_length=50, blank=True, null=True)
-    avatar = models.ImageField(upload_to="avatar/", blank=True, null=True)
+    avatar = models.URLField(blank=True, null=True)
     mini_about = models.TextField(blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
@@ -30,7 +30,7 @@ class About(models.Model):
     title = models.CharField(max_length=20, blank=True, null=True)
     description1 = models.TextField(blank=False, null=True)
     description2 = models.TextField(blank=False, null=True)
-    about_avatar = models.ImageField(blank=True, null=True)
+    about_avatar = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -40,7 +40,7 @@ class Projects(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     skill = models.TextField(max_length=230, blank=True, null=True)
     link = models.URLField(blank=True, null=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -53,7 +53,7 @@ class Contact(models.Model):
     location = models.CharField(max_length=50, blank=True, null=True)
     msg = models.TextField(max_length=100, blank=True, null=True)
     link = models.URLField(blank=True, null=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
 
     github = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
